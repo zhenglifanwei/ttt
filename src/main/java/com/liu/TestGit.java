0d5bcd9cdfe2c2983ca756af6f901afeb4b88d52
liu <liu@123>
2024-01-13 20:42:11 +0800

    master分支更新

--- src/main/java/com/liu/TestGit.java
+++ src/main/java/com/liu/TestGit.java
@@ -2,6 +2,8 @@ package com.liu;
 
 public class TestGit {
     public static void main(String[] args) {
+
         System.out.println("hello git");
+        System.out.println("master fenzhi gengxin");
     }
 }
