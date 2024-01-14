41ff85cce22947091b18c6e7c0d1dbf59f321af4
liu <liu@123>
2024-01-14 10:16:22 +0800

    master分支更新

--- src/main/java/com/liu/TestGit.java
+++ src/main/java/com/liu/TestGit.java
@@ -2,9 +2,6 @@ package com.liu;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello test branch");
-        System.out.println("zaici gengxin");
 
     }
 }
